e88d2dcaa160a2f9c11c8c6ddf915c9cc03f8961
mschuldt <mbschuldt@gmail.com>
2015-04-13 23:06:01 -0700

    move some functions into new file 'util.py'

--- interpreter.py
+++ interpreter.py
@@ -6,6 +6,7 @@ import time
 import requests
 import json
 from operator import *
+from util import *
 
 USE_MSGPACK = False #otherwise use json
 # => would we ever need this?
@@ -67,32 +68,6 @@ def def_prim(name, fn):
     bind(primitives, name, fn)
 
 ################################################################################
-#The following two function where taken from:
-#  http://stackoverflow.com/questions/956867/how-to-get-string-objects-instead-of-unicode-ones-from-json-in-python
-def _decode_list(data):
-    rv = []
-    for item in data:
-        if isinstance(item, unicode):
-            item = item.encode('utf-8')
-        elif isinstance(item, list):
-            item = _decode_list(item)
-        elif isinstance(item, dict):
-            item = _decode_dict(item)
-        rv.append(item)
-    return rv
-def _decode_dict(data):
-    rv = {}
-    for key, value in data.iteritems():
-        if isinstance(key, unicode):
-            key = key.encode('utf-8')
-        if isinstance(value, unicode):
-            value = value.encode('utf-8')
-        elif isinstance(value, list):
-            value = _decode_list(value)
-        elif isinstance(value, dict):
-            value = _decode_dict(value)
-        rv[key] = value
-    return rv
 ################################################################################
 def load_json(s):
     return json.loads(s, object_hook=_decode_dict)
