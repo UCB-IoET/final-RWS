fe4bc85aeb621fd9240e6a5d5ce3822fba9cc004
mschuldt <mbschuldt@gmail.com>
2015-04-14 20:56:04 -0700

    update test program

--- dummy_source.py
+++ dummy_source.py
@@ -4,70 +4,40 @@ import time
 
 import socket
 
-"""
-{'type':'prim',
-            'name':'print',
-            'args':[{'type': 'binop',
-                     'op': '+',
-                     'left': {'type': 'lit', 'ltype': 'int', 'val': n},
-                     'right': {'type': 'lit', 'ltype': 'int', 'val': n*n}}]}
-"""
+# [0:4] -0-[2:+]-2-[3:print]
+# [1:8] -1-/
 
-def make_ast(n):
-    # return {'type':'prim',
-    #         'name':'print',
-    #         'args':[{'type': 'binop',
-    #                  'op': '+',
-    #                  'left': {'type': 'lit', 'ltype': 'int', 'val': n},
-    #                  'right': {'type': 'lit', 'ltype': 'int', 'val': n*n}}]}
+program = {'type':'program',
+           'password': 'password',
+           'initial': ['0', '1'],
+           'connections':{'0' : ['2'],
+                          '1' : ['2'],
+                          '2' : ['3']},
+           'nodes':{'0' : {'type': 'literal',
+                           'val': 4,
+                           'out': '0'},
+                    '1' : {'type': 'literal',
+                           'val': 8,
+                           'out': '1'},
+                    '2' : {'type': 'binop',
+                           'op': '+',
+                           'left': '0',
+                           'right': '1',
+                           'out': '2'},
+                    '3' : {'type': 'call',
+                           'name': 'print',
+                           'in': ['2']}}}
 
-    x = 'http://169.229.223.190:8081/data/buildinggeneral/plugstrip0/outlet2/on_act?state={0}'
-    on = x.format(1)
-    off = x.format(0)
-    return {'type': 'begin',
-            'exprs': [{'type': 'prim',
-                       'name': 'put_request',
-                       'args': [{'type': 'lit', 'ltype': 'str',
-                                 'val': on}]},
-                      {'type': 'prim',
-                       'name': 'put_request',
-                       'args': [{'type': 'lit', 'ltype': 'str',
-                                 'val': off}]}]}
 
-#IP_6 = "fe80::201:c0ff:fe15:8419"
-IP = "2001:470:4956:1::1" #from ifconfig -> eth1
-
-#IP = "2607:f140:400:a009:2c5b:3fd4:a97a:7e3al" #michaels laptop
-#IP = "2001:470:4956:1:5819:1468:cea9:bb21"
-#IP = "2607:f140:400:a009:acd7:a6b7:a1e4:8a69"
-IP = "2001:470:4956:1:358e:d654:6a9e:b23c"
-
+IP = "2607:f140:400:a001:189e:1858:2c10:b972"
 PORT=8888
 BUFFER_SIZE = 1024
 UDP_PORT = 1263
 
 s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#s.connect((IP, PORT))
 s.connect((IP, UDP_PORT))
-i = 0
-while True:
-    i += 1
-    data = {"key":"password",
-            "ast" : make_ast(i)}
-    resp = s.send(json.dumps(data))
-    print(">>> " + str(resp))
-    time.sleep(2)
 
+resp = s.send(json.dumps(program))
 
-"""
-source:
-  http://proj.storm.pm:8081/data/
-
-archiver:
-  http://shell.storm.pm:8079/api/query
-
-"""
-
-
+print(resp)
