c5c1cf7f22633466f0e46f3a8cd7279adb541ede
Kavan Sikand <kavanthewise@yahoo.com>
2015-04-15 10:17:04 -0700

    SMAP interface handles parsing metadata

--- ioet-view/www/js/RWSNode.js
+++ ioet-view/www/js/RWSNode.js
@@ -20,26 +20,13 @@ function RWSNode(type, infoDict) {
 	this.outputs = [];
 	this.infoDict = infoDict;
 
-	//potentially move this if statement into the SMAP interface
-	//so we don't have any SMAP parsing in here
-	if(type === TYPES[0]) { //SMAP
-		if ("Name" in this.infoDict) {
-			this.name = this.infoDict["Name"];
-		} else {
-			this.name = this.infoDict["Path"];
-		}
-	} else if(type === TYPES[1]) { //SVCD
-
-	}
-
-
 	this.draw = function(context) {
         context.fillStyle="rgba(150, 150, 150, 1)";
 		context.fillRect(this.x, this.y, nodeWidth, nodeHeight);
 		drawString(context, this.name + '\n' + this.description, this.x + 5, this.y + 10, "#333333", 0, 'serif', 12);
-       // context.fillText(this.name, this.x + nodeWidth/2 - context.measureText(this.name).width/2, this.y + 10);
-        //context.fillText(this.description, this.x + nodeWidth/2 - context.measureText(this.description).width/2, this.y + 20);
+       
         context.fillStyle="rgba(50, 50, 50, .7)";
+        
         //draw triangles for inputs
         if(this.inputs.length > 0) {
         	var interval = (nodeWidth - (ioSize*2)*this.inputs.length) / (this.inputs.length + 1);
--- ioet-view/www/js/RWSSMAPInterface.js
+++ ioet-view/www/js/RWSSMAPInterface.js
@@ -3,66 +3,71 @@ var NEW_LINE = '<br/>';
 function RWSSMAPInterface(root_url, available_nodes) {
 	var smap = this;
 	smap.entries = []
-    this.find_nodes = function() {
-        
-        //first sensors
-        $.ajax({
-            type: 'POST',
-            url: root_url,
-            data: 'select * where Metadata/Type="Sensor"',
-            success: function(data) {
-                data.forEach(function(datum) {
-                smap.add_entry(datum);
-                });
-            }
-            ,
-           async:false
-        });
-
-        //then actuators
-        $.ajax({
-           type: 'POST',
-           url: root_url,
-           data: 'select * where has Actuator',
-           success: function(data) {
-               data.forEach(function(datum) {
-                smap.add_entry(datum);
-                });
-           }
-           ,
-           async:false
-       });
-	};
-
-	this.add_entry = function(entry) {
-        //need to check for duplicates
-		this.entries.push(entry);
-	}
-    
-    this.select_entry = function(entry) {
-        var node = new RWSNode("SMAP", entry);
-
-        if(entry['Actuator']) {
-          node.inputs.push(0);
-        } else if(entry['Metadata']['Type'] == 'Sensor') {
-          node.outputs.push(0);
-          node.description = 'Sensor: ' + entry['Metadata']['Sensor'] + '\n'
-          node.description += 'Units: ' + entry['Properties']['UnitofMeasure'];
+  this.find_nodes = function() {
+    //first sensors
+    $.ajax({
+        type: 'POST',
+        url: root_url,
+        data: 'select * where Metadata/Type="Sensor"',
+        success: function(data) {
+            data.forEach(function(datum) {
+            smap.add_entry(datum);
+            });
         }
-        //need to check for duplicates
-        available_nodes.push(node);
-    }
+        ,
+       async:false
+    });
 
-    this.html_for_entry = function(entry) {
-    	str = '';
-    	if(entry['Actuator']) {
-    		str += 'Actuator' + NEW_LINE;
-    		str += 'Path: ' + entry['Path'] + NEW_LINE;
-    	} else if(entry['Metadata']['Type'] == 'Sensor') {
-    		str += 'Sensor: ' + entry['Metadata']['Sensor'] + NEW_LINE;
-    		str += 'Units: ' + entry['Properties']['UnitofMeasure'] + NEW_LINE;
-    		str += 'Path: ' + entry['Path'] + NEW_LINE;
-    	}
-    	return str;
-    }
+    //then actuators
+    $.ajax({
+       type: 'POST',
+       url: root_url,
+       data: 'select * where has Actuator',
+       success: function(data) {
+           data.forEach(function(datum) {
+            smap.add_entry(datum);
+            });
+       }
+       ,
+       async:false
+    });
+  };
+
+  this.add_entry = function(entry) {
+        //need to check for duplicates
+  	this.entries.push(entry);
+  }
+    
+  this.select_entry = function(entry) {
+      var node = new RWSNode("SMAP", entry);
+      if(entry['Metadata']['Name']) {
+        node.name = entry['Metadata']['Name'];
+      }
+      node.uuid = entry['uuid'];
+      if(entry['Actuator']) {
+        node.inputs.push('actuation');
+        if(entry['Metadata']['Type'] == 'Reading') {
+          node.outputs.push('reading');
+        }
+      } else if(entry['Metadata']['Type'] == 'Sensor') {
+        node.outputs.push('sensor');
+        node.description = 'Sensor: ' + entry['Metadata']['Sensor'] + '\n'
+        node.description += 'Units: ' + entry['Properties']['UnitofMeasure'];
+      }
+      //need to check for duplicates
+      available_nodes.push(node);
+  }
+
+  this.html_for_entry = function(entry) {
+  	str = '';
+  	if(entry['Actuator']) {
+  		str += 'Actuator' + NEW_LINE;
+  		str += 'Path: ' + entry['Path'] + NEW_LINE;
+  	} else if(entry['Metadata']['Type'] == 'Sensor') {
+  		str += 'Sensor: ' + entry['Metadata']['Sensor'] + NEW_LINE;
+  		str += 'Units: ' + entry['Properties']['UnitofMeasure'] + NEW_LINE;
+  		str += 'Path: ' + entry['Path'] + NEW_LINE;
+  	}
+  	return str;
+  }
 }
