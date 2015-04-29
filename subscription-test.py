da405595728a0b12d440f33c4c50febb006d05c5
mschuldt <mbschuldt@gmail.com>
2015-04-28 18:43:58 -0700

    smap subscription tests

---
+++ subscription-test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import requests
+import json
+
+
+#curl -XPOST -d 'Metadata/SourceName = "Gabe Sine Wave Driver"' http://shell.storm.pm:8079/republish
+
+from ws4py.client.threadedclient import WebSocketClient
+
+class Subscription(WebSocketClient):
+    def __init__(self, url, select, cb):
+        super(Subscription, self).__init__(url)
+        self.select = select
+        self.cb = cb
+
+    def opened(self):
+        self.send(self.select)
+
+    def closed(self, code, reason=None):
+        print "Closed down", code, reason
+
+    def received_message(self, m):
+        self.cb(m)
+
+def cb(m):
+    print(m)
+
+try:
+
+    ws = Subscription('ws://shell.storm.pm:8078/republish',
+                      "Metadata/SourceName = 'Gabe Sine Wave Driver'",
+                      cb)
+    ws.connect()
+    ws.run_forever()
+except KeyboardInterrupt:
+    ws.close()
