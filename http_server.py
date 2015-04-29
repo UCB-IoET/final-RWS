5ff4c4f14bd15c8cfdf80c610f5ae0f728a5fb57
mschuldt <mbschuldt@gmail.com>
2015-04-28 20:50:40 -0700

    interpreter smap subscriptions

--- http_server.py
+++ http_server.py
@@ -12,7 +12,7 @@ import json
 from interpreter import run_program
 from interpreter import _node_configs
 
-PORT = 1445
+PORT = 1455
 
 def client_thread(program, addr, n_threads):
     #stream some data to the archiver
